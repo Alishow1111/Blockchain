8f9e2e44e6236dbcd64af44dcf7292fb98154bfd
Alishow1111 <78172307+Alishow1111@users.noreply.github.com>
2024-01-31 23:14:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from blockchain import Blockchain
 
-# Example usage
 blockchain = Blockchain()
 blockchain.add_new_transaction("Ali pays Tom 1 BTC")
 blockchain.mine()
 blockchain.add_new_transaction("David pays Zara 0.5 BTC")
 blockchain.mine()
 
-# Display the blockchain
 for block in blockchain.chain:
     print(f"Index: {block.index}")
     print(f"Transactions: {block.transactions}")
